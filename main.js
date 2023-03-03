33f6f31d3b0edb59b3226f1b383c2de2c6fb16aa
samirmld <samir.moualed11@gmail.com>
2023-03-03 10:02:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+ 
+      var swiper = document.querySelectorAll(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+   
+   
+
